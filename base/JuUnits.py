bb7dc418a482f8e24c9907d48167cc49909cdd00
kai_kai03 <kai_kai03@hotmail.com>
2022-03-01 19:16:59 +0800

    #

--- base/JuUnits.py
+++ base/JuUnits.py
@@ -227,3 +227,18 @@ def date_range(begin_date,end_date):
         begin_date += timedelta(days=1)
     return date_list
 
+
+def get_root_path(project_name='IndicatorLab'):
+    """
+    获取当前项目根路径
+    :param project_name: 项目名称
+    :return: 指定项目的根路径
+    """
+    p_name = 'IndicatorLab' if project_name is None else project_name
+    project_path = os.path.abspath(os.path.dirname(__file__))
+    # Windows
+    if project_path.find('\\') != -1: separator = '\\'
+    # Mac、Linux、Unix
+    if project_path.find('/') != -1: separator = '/'
+    root_path = project_path[:project_path.find(f'{p_name}{separator}') + len(f'{p_name}{separator}')]
+    return root_path

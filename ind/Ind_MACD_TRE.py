ae0af7df62ad2eefb8cbecd6a3b0f6335de70d52
kai_kai03 <kai_kai03@hotmail.com>
2023-08-19 23:17:06 +0800

    #

--- ind/Ind_MACD_TRE.py
+++ ind/Ind_MACD_TRE.py
@@ -5,78 +5,84 @@ module_path = os.path.abspath(os.path.join('..'))
 if module_path not in sys.path: 
     sys.path.append(module_path)
 
-
 import numpy as np
 import pandas as pd
 
 import QUANTAXIS as QA
 
-import Ind_Model_Base
+import tools.Sample_Tools as smpl
+from base.JuUnits import excute_for_multidates
 
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 
+from numba import jit
+
+@jit(nopython=True)
+def calc_deviation(MACD_origin_rolldata, window):
+    x = MACD_origin_rolldata
+    if x.shape[0] < window:
+        return np.nan,np.nan
+    x_t = x.T
+    MACD = x_t[0]
+    origin = x_t[1]
+    if MACD is None or origin is None:
+        return np.nan,np.nan
+    if MACD is np.nan or origin is np.nan:
+        return np.nan,np.nan
+    
+    last_MACD = np.argsort(MACD)[-1]
+    last_origin = np.argsort(origin)[-1]
+
+    diff = 0.0
+    if last_origin < 1.0:#新低，底背离
+        diff = np.abs(last_MACD - last_origin)
+        
+    if last_origin > 8.0:#新高,顶背离
+        diff =  np.abs(last_MACD - last_origin)*-1.0
+    return 0,diff
 
 
-class MACD_JCSC(Ind_Model_Base.Ind_Model):
-    optimum_param={'valid':False, 'main':'MACD', 'desition_direct':1, 'freq':'d','neutralize':{'enable':False,'static_mv':True}}
-    
-    def __init__(self,data, frequence=QA.FREQUENCE.DAY):
-        super().__init__(data, 'MACD', frequence)
-        
 
-    def on_set_params_default(self):
-        return {'SHORT':12,'LONG':26,'M':9}
-    
-        
-    def on_indicator_structuring(self, data):
-        #return data.add_func(self.MACD_JCSC,**self.pramas)
-        return self.excute_for_multicode(data, self.kernel, **self.pramas)
-    
-    def on_desition_structuring(self, data, ind_data):
-        """
-        1.DIF向上突破DEA,res为1，买入信号参考。
-        2.DIF向下跌破DEA,res为-1，卖出信号参考。
-        """
-        return pd.DataFrame({'res':ind_data['CROSS_JC'] + ind_data['CROSS_SC']*-1})
-        
-    def kernel(self,dataframe,SHORT=12,LONG=26,M=9):
-        CLOSE=dataframe.close
-        DIFF =QA.EMA(CLOSE,SHORT) - QA.EMA(CLOSE,LONG)
+def MACD_JCSC(data_series,SHORT=12,LONG=26,M=9,deviate_window=10):    
+    def kerrel(stock_single):
+        DIFF = QA.EMA(stock_single,SHORT) - QA.EMA(stock_single,LONG)
         DEA = QA.EMA(DIFF,M)
-        MACD =2*(DIFF-DEA)
+        MACD = 2*(DIFF-DEA)
+
+        CROSS_JC = QA.CROSS(DIFF,DEA)
+        CROSS_SC = QA.CROSS(DEA,DIFF)
+        CROSS = CROSS_JC + CROSS_SC*-1
         
-        if self.fast_mode:
-            return pd.DataFrame({'main':MACD}) 
+        tmp_df = pd.DataFrame({'MACD':MACD,'origin_data':stock_single},columns=['MACD','origin_data'])
+        deviation = tmp_df.dropna().rolling(deviate_window, method='table').apply(lambda x:calc_deviation(x,deviate_window), raw=True, engine='numba')['origin_data']
 
-        CROSS_JC=QA.CROSS(DIFF,DEA)
-        CROSS_SC=QA.CROSS(DEA,DIFF)
+        
+        return pd.DataFrame({'MACD':MACD,'DIFF':DIFF,'DEA':DEA,'CROSS':CROSS,'DEVIATE':deviation})
 
-        return pd.DataFrame({'MACD':MACD,'DIFF':DIFF,'DEA':DEA,'MACD':MACD,'CROSS_JC':CROSS_JC,'CROSS_SC':CROSS_SC})
+    return excute_for_multidates(data_series, kerrel, level=1)
 
-    def plot(self,figsize=(1120/72,420/72)):
-        fig = plt.figure(figsize=figsize)
-        groups = self.ind_df.groupby(level=1)
-        for idx,item in enumerate(groups):
-            inds_ = item[1].reset_index('code',drop=True)
-            ax = fig.add_subplot(len(groups),1,idx+1)
-            
-            
-            ##axis不转成字符串的话，bar和line的x轴有时候对不上，原因未知
-            
-            formater = '%Y%m%d' if self.is_low_frequence else '%Y%m%d %H%M%S'
-            index_ = [pd.to_datetime(x).strftime(formater) for x in inds_.index.values]
-#             d = item[1].reset_index(('date','code'),drop=True)
 
-            ax.set_title(item[0],color='r', loc ='left', pad=-10) 
+def MACD_plot(MACD_df,low_frequence=True):
+    groups = MACD_df.groupby(level=1)
+    assert len(groups)<20,'不允许超过20组'
+    fig = plt.figure(figsize=(2120/72,220*len(groups)/72))
+    for idx,item in enumerate(groups):
+        inds_ = item[1].reset_index('code',drop=True)
+        
+        ax = fig.add_subplot(len(groups),1,idx+1)
+        ax.xaxis.set_major_locator(ticker.MaxNLocator(10))
+        
+        ##axis不转成字符串的话，bar和line的x轴有时候对不上，原因未知
+        formater = '%Y%m%d' if low_frequence else '%Y%m%d %H%M%S'
+        index_ = [pd.to_datetime(x).strftime(formater) for x in inds_.index.values]
+        #d = item[1].reset_index(('date','code'),drop=True)
+        ax.set_title(item[0],color='r', loc ='left', pad=-10) 
+        if 'DIFF' in MACD_df.columns and 'DEA' in MACD_df.columns: 
             DD = inds_[['DIFF','DEA']]
             DD.index = index_
             DD.plot(kind='line', ax=ax)
-            macd = inds_['MACD']
-            macd.index = index_
-            macd.plot(kind='bar', ax=ax)
-            ax.xaxis.set_major_locator(ticker.MaxNLocator(10))
-            plt.xticks(rotation = 0)
-    
-
+        macd = inds_['MACD']
+        ax.bar(index_,macd.values)
+        plt.xticks(rotation = 0)
     
